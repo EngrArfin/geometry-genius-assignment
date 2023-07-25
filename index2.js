92e5c24439e9ed809ddad2fa1bcf3e277e4b2576
EngrArfin <arfin.cse.green.edu.bd.gmail.com>
2023-07-25 08:31:07 +0600

    final up datee

---
+++ index2.js
@@ -0,0 +1,6 @@
+function multiply() {
+    var w = document.getElementById("w").value;
+    var l = document.getElementById("l").value;
+    var result2 = w * l;
+    document.getElementById("result").innerHTML = " " + result2;
+  }
