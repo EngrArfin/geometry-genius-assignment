5660117555044c72f8da85a0ed4b43094586a271
EngrArfin <arfin.cse.green.edu.bd.gmail.com>
2023-08-05 10:07:14 +0600

    final up

---
+++ index1.js
@@ -0,0 +1,13 @@
+function multiply() {
+    var b = document.getElementById("b").value;
+    var h = document.getElementById("h").value;
+    var result = 0.5 * b * h;
+    document.getElementById("result").innerHTML = " " + result;
+  }
+
+  function multiply1() {
+    var w = document.getElementById("w").value;
+    var l = document.getElementById("l").value;
+    var result2 = w * l;
+    document.getElementById("result").innerHTML = " " + result2;
+  }
